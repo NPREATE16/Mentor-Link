91ac7e42c926ccc3887b306f8c2f8286ac6dcd82
khiem918 <khiemtrong2k5@gmail.com>
2025-10-28 22:08:51 +0700

    update database cloud_hosting

--- webserver/database.js
+++ webserver/database.js
@@ -1,10 +1,14 @@
 import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const dbPool = mysql.createPool({
-  host: "localhost",
-  user: "root",         
-  password: "root", 
-  database: "BTL_CNPM", 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,         
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT, 
 });
 
 export default dbPool;
